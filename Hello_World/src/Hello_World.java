61cf2869086bca6b82eb6818ebb3d6bd20db2505
sshaoo <18kaos49@millshighsch.org>
2017-08-18 14:42:00 -0700

    First Code
    Hello World! Test

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,14 @@
+/**
+ * Introduction From Me
+ * 
+ * @Shao Ching Kao
+ * @8/18/17
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Shao"); 
+        //Display the string
+    }
+}
