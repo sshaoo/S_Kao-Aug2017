79c62fc419d7dc40ec059cd5e7cfb755194a8223
sshaoo <18kaos49@millshighsch.org>
2017-08-29 08:07:52 -0700

    Added a new project named Pokemon World. Created a system of inputs to display pikachu greeting the user.

---
+++ Pokemon World/src/PokemonWorld.java
@@ -0,0 +1,9 @@
+
+public class PokemonWorld {
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of pokemon");
+		System.out.println("\t\t\t (\\\"__/)");
+		System.out.println("\t\t\t (o^.^)");
+		System.out.print("\t\t        z(_(\")(\")");
+	}
+}
