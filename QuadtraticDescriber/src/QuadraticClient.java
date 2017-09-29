7e531c4128a3fc125123a319c255892f2b3c7d82
sshaoo <18kaos49@millshighsch.org>
2017-09-29 08:22:02 -0700

    fixed little details

--- QuadtraticDescriber/src/QuadraticClient.java
+++ QuadtraticDescriber/src/QuadraticClient.java
@@ -1,8 +1,11 @@
-
+/* Shao Ching Kao
+ * September 26th, 2017
+ * This class contains a main method and handles interactions with the user. 
+ * It calls the method QuadrDescriber with the appropriate arguments. 
+ */
 public class QuadraticClient {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
