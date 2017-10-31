e4fd139f1471d2fa373a20f702a2dfa11924221d
sshaoo <31105385+sshaoo@users.noreply.github.com>
2017-10-30 21:27:17 -0700

    finished with mini array lab

--- Array Mini Lab/src/LotsOfCopies.java
+++ Array Mini Lab/src/LotsOfCopies.java
@@ -9,19 +9,38 @@ import java.util.Arrays;
 
 public class LotsOfCopies {
 	public static void main (String[] args) {
+		
+		//part 1
 		int num = 7;
-		String strMain = "APCS";
+		String strMain = "APCS is not fun";
 		int[] arrMain = {1, 2, 3, 4, 5}; 
-		changeMe (num, strMain, arrMain);
+		changeMe(num, strMain, arrMain);
 		System.out.println (num);
 		System.out.println (strMain);
-		System.out.println (arrMain);
 		System.out.println (Arrays.toString(arrMain));
 		
+		//part 2
+		int a = 2;
+		int b = a;
+		a = 3;
+		System.out.println (a);
+		System.out.println (b);
+		String c = "first period APCS is better than second period";
+		String d = c;
+		c = "This is boring";
+		System.out.println (c);
+		System.out.println (d);
+		int[] e = {0, 1, 2};
+		int[] f = e;
+		e[1] = 10;
+		e[2] = 20;
+		System.out.println(Arrays.toString(f));
+		System.out.println(Arrays.toString(e)); 
 	}
 	public static void changeMe (int x, String str, int[] arr) {
 		x += 9;
-		str = "APCS is fun :(";
+		str = "APCS is fun";
 		arr [2] = 7;	
+		
 	}
 }
