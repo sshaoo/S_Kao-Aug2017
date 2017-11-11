8b475c488583ecf5663068cbb11258c9fd098927
sshaoo <31105385+sshaoo@users.noreply.github.com>
2017-11-11 14:57:07 -0800

    started on checkpoint two

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -11,12 +11,19 @@ public class FracCalc {
 
     public static void main(String[] args) {
     		Scanner console = new Scanner (System.in);
-    		String equation = console.nextLine();
+    		String equation;
+    		while (true) {
+    			System.out.println("Type quit to end. Otherwise, enter your equation.");
+    			equation = console.nextLine();
+    			System.out.println(produceAnswer(equation));
+    			if (equation.toLowerCase().equals("quit")) {
+    				break;
+    			}
+    		}
     		System.out.println(produceAnswer(equation));
     }
     		
     // TODO: Read the input from the user and call produceAnswer with an equation
-   
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
     // This function takes a String 'input' and produces the result
@@ -26,18 +33,36 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-   
-    public static String produceAnswer(String input) {
-        
-    	// TODO: Implement this function to produce the solution to the input
-        
-    		String[] inputSplit = input.split(" ");
-        String operandOne = inputSplit [0];
-        String operator = inputSplit [1];
-        String operandTwo = inputSplit [2];
+    // TODO: Implement this function to produce the solution to the input
+    
+    public static String produceAnswer (String userInput) {
+		//checkpointOne:
+    		String[] answerParseInput = parseInput(userInput);
+        String operandOne = answerParseInput[0];
+        String operator = answerParseInput[1];
+        String operandTwo = answerParseInput[2];
         return operandTwo;
+        //checkpointTwo:
+        
+        
     }
-
+    
     // TODO: Fill in the space below with any helper methods that you think you will need
     
+    // checkpointOne:
+    public static String[] parseInput (String input) {
+    		String[] inputSplit = input.split(" ");
+    		return inputSplit;
+    }
+    
+    public static int[] parseOperand (String operand) {
+    		String[] partsSplit = operand.split("_");
+    		String[] fractionSplit = partsSplit[1].split("/");
+    		String[] operandSplit = {partsSplit[0], fractionSplit[0], fractionSplit[1]};
+    		int[] operandSplitInt = new int [3];
+    		for (int i = 0; i < operandSplitInt.length; i++) {
+    			operandSplitInt[i] = Integer.parseInt(operandSplit[i]);
+    		}
+    		return operandSplitInt;
+    }
 }
