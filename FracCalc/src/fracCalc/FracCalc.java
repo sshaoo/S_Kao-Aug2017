f21d8ebc68da28c8ef65be1a338ff49017624e6b
sshaoo <31105385+sshaoo@users.noreply.github.com>
2017-11-24 19:03:45 -0800

    finished checkpoint 3

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -11,17 +11,14 @@ public class FracCalc {
 
     public static void main(String[] args) {
     		Scanner console = new Scanner (System.in);
-    		String equation = "";
-    		boolean test = false;
-    		while (!test == true) {
-    			System.out.println("Type quit to end. Otherwise, enter your equation.");
-    			equation = console.nextLine();
+    		System.out.println("Please enter your equation.");
+    		String equation = console.nextLine();
+    		while (!equation.equals("quit")) {
     			System.out.println(produceAnswer(equation));
-    			if (equation.toLowerCase().equals("quit")) {
-    				test = false;
+    			System.out.println("Type \"quit\" with no capital letters to end. Otherwise, enter your equation.");
+    			equation = console.nextLine();
     			}
-    		}
-    		System.out.println(produceAnswer(equation));
+    		console.close();
     }
     		
     // TODO: Read the input from the user and call produceAnswer with an equation
@@ -38,10 +35,10 @@ public class FracCalc {
     
     public static String produceAnswer (String userInput) {
     	
-    		String[] answerParseInput = parseInput(userInput);
-        String operandOne = answerParseInput[0];
-        String operator = answerParseInput[1];
-        String operandTwo = answerParseInput[2];
+    		String[] inputSplit = userInput.split(" ");
+        String operandOne = inputSplit[0];
+        String operator = inputSplit[1];
+        String operandTwo = inputSplit[2];
         int[] parseOperandOne = parseOperand(operandOne);
         int[] parseOperandTwo = parseOperand(operandTwo);
         int[] improperFracOperandOne = toImproperFrac(parseOperandOne);
@@ -67,11 +64,6 @@ public class FracCalc {
     
     // TODO: Fill in the space below with any helper methods that you think you will need
     
-    public static String[] parseInput (String input) {
-    		String[] inputSplit = input.split(" ");
-    		return inputSplit;
-    }
-    
     public static int[] parseOperand (String operand) {
     		int[] equationSplit = new int[3];
     		String[] fractionSplit = null;
@@ -94,13 +86,16 @@ public class FracCalc {
     		return equationSplit;
     }
     
-    public static int[] toImproperFrac(int[] operand){
+    public static int[] toImproperFrac(int[] operand) {
+    		if (operand[0] < 0) {
+    			operand[1] *= -1;
+    		}
 		operand[1] = operand[2] * operand[0] + operand[1];
 		int[] improperFraction = {operand[1], operand[2]};
 		return improperFraction;
     }
     
-    public static int[] commonDenominator(int[] operandOne, int[] operandTwo){
+    public static int[] commonDenominator(int[] operandOne, int[] operandTwo) {
 		int leftNumerator = operandOne[0] * operandTwo[1];
 		int rightNumerator = operandTwo[0] * operandOne[1];
 		int denominator = operandOne[1] * operandTwo[1];
@@ -108,19 +103,19 @@ public class FracCalc {
 		return twoCommonFractions;
     }
     
-    public static String addition(int[] expression){
+    public static String addition(int[] expression) {
 		int intNumerator = expression[0] + expression[1];
 		String stringNumerator = Integer.toString(intNumerator);
 		String answerString = stringNumerator + "/" + expression[2];
 		return answerString;
 	}
-	public static String subtraction(int[] expression){
+	public static String subtraction(int[] expression) {
 		int intNumerator = expression[0] - expression[1];
 		String stringNumerator = Integer.toString(intNumerator);
 		String answerString = stringNumerator + "/" + expression[2];
 		return answerString;
 	}
-	public static String multiplication(int[] expression){
+	public static String multiplication(int[] expression) {
 		int intNumerator = expression[0] * expression[1];
 		int intDenominator = expression[2] * expression[2];
 		String stringNumerator = Integer.toString(intNumerator);
@@ -128,7 +123,7 @@ public class FracCalc {
 		String answerString = stringNumerator + "/" + stringDenominator;
 		return answerString;
 	}
-	public static String division(int[] expression){
+	public static String division(int[] expression) {
 		int intNumerator = expression[0] * expression[2];
 		int intDenominator = expression[1] * expression[2];
 		String stringNumerator = Integer.toString(intNumerator);
