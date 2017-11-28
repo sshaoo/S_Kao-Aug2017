22d0dd1bac2a7e27ded19a13fcee11e47c7e03e1
sshaoo <18kaos49@millshighsch.org>
2017-11-28 09:29:16 -0800

    Started Code Magnet

---
+++ Code Magnet/src/DrumKit.java
@@ -0,0 +1,15 @@
+/* Shao Ching Kao
+ * Ms.Dreyer's first period AP computer science
+ * November 7th, 2017
+ * This is where the work for Drum Kit will be stored
+ */
+public class DrumKit {
+	boolean topHat = true;
+	boolean snare = true;
+	void playSnare() {
+		System.out.println ("bang bang ba-bang");
+	}
+	void playTopHat() {
+		System.out.println ("ding ding da-ding");
+	}
+}
---
+++ Code Magnet/src/DrumKitTestDrive.java
@@ -0,0 +1,21 @@
+/* Shao Ching Kao
+ * Ms.Dreyer's first period AP computer science
+ * November 7th, 2017
+ * This is where the space for Drum Kit Test Drive
+ */
+
+import java.awt.*;
+
+class DrumKitTestDrive {
+	public static void main(String[] args) {
+		DrumKit d = new DrumKit();
+		d.playSnare;
+		d.playTopHat;
+		d.snare = false;
+		if (d.snare == true) {
+			d.playSnare();
+		}
+	}
+}
+
+
