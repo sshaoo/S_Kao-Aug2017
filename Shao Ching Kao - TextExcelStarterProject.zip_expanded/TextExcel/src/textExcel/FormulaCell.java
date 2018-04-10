14f022678466cd2cf079f0682c3fe0e6d4e00db5
sshaoo <31105385+sshaoo@users.noreply.github.com>
2018-04-09 21:25:30 -0700

    done with checkpoint 4

--- Shao Ching Kao - TextExcelStarterProject.zip_expanded/TextExcel/src/textExcel/FormulaCell.java
+++ Shao Ching Kao - TextExcelStarterProject.zip_expanded/TextExcel/src/textExcel/FormulaCell.java
@@ -7,13 +7,23 @@
 package textExcel;
 
 public class FormulaCell extends RealCell {
-
+	
  	public FormulaCell(String input) {
 		super(input);
+		
 	}
 
 	public String abbreviatedCellText() {
-		return null;
+		String answer = "" + getDoubleValue();
+		if (answer.length() > 10) {
+			return answer.substring(0, 10);
+		}
+		else {
+			for (int i = answer.length(); i < 10; i++) {
+				answer += " ";
+			}
+			return answer;
+		}
 	}
 
 	public String fullCellText() {
@@ -21,22 +31,22 @@ public class FormulaCell extends RealCell {
 	}
 
 	public double getDoubleValue() {
-		String inputSplit[] = getInput().split(" ");
+		String inputSplit[] = getInput().substring(getInput().indexOf("(") + 2, getInput().indexOf(")") - 1).split(" ");
 		double answer = Double.parseDouble(inputSplit[0]);
-		for (int i = 0; i < inputSplit.length; i++) {
-			if (inputSplit[i] == "+") {
+		for (int i = 1; i < inputSplit.length; i += 2) {
+			if (inputSplit[i].equals("+")) {
 				answer += Double.parseDouble(inputSplit[i + 1]);
 			}
-			else if (inputSplit[i] == "-") {
+			else if (inputSplit[i].equals("-")) {
 				answer -= Double.parseDouble(inputSplit[i + 1]);
 			}
-			else if (inputSplit[i] == "*") {
+			else if (inputSplit[i].equals("*")) {
 				answer *= Double.parseDouble(inputSplit[i + 1]);
 			}
-			else if (inputSplit[i] == "/") {
+			else if (inputSplit[i].equals("/")) {
 				answer /= Double.parseDouble(inputSplit[i + 1]);
 			}
-			return answer;
 		}
+		return answer;
 	}
 }
